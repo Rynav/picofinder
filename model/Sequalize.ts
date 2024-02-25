6a8b9078ce22106238fc64cf4cbcdd9e6fa3e426
Rynav <rynav@rynav.xyz>
2024-02-26 00:07:52 +0100

    Funny path removed

--- model/Sequalize.ts
+++ model/Sequalize.ts
@@ -4,7 +4,7 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
   dialect: 'sqlite', // Adjust the dialect based on your database type (mysql, postgres, etc.)
-  storage: 'C:\\Users\\Rynav\\Desktop\\picofinder1\\picodata\\aaaaaaaa.db', // Adjust the storage path based on your project structure
+  storage: '../picodata/aaaaaaaa.db', // Adjust the storage path based on your project structure
   logging: false, // Set to true if you want to log SQL queries to the console
 });
 
