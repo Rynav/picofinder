632af6fa6f00fa2db96d34b85aa704fe3ad2edbe
Rynav <rynav@rynav.xyz>
2024-02-28 14:32:31 +0100

    Fixed uri encoding issues

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
   function fetchData() {
 
-    let query = Buffer.from(JSON.stringify({ encoder: (customEncoder ? customEncoder : encoder), title: title, lengthFrom: lengthFrom, lengthTo: lengthTo, uploadYear: year, uploadMonth: month }), "utf-8").toString("base64")
+    let query = encodeURIComponent(Buffer.from(JSON.stringify({ encoder: (customEncoder ? customEncoder : encoder), title: title, lengthFrom: lengthFrom, lengthTo: lengthTo, uploadYear: year, uploadMonth: month }), "utf-8").toString("base64"))
 
     console.log(query);
     if(encoder.toString().length <= 1 && year.length <= 2 && month.length <= 2 && title.length <= 3){
